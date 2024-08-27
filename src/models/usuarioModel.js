22b5196afe8dec966412750880114eca76a5c36c
douglas ferreira <douglasstafussa15@gmail.com>
2024-08-26 21:28:16 -0300

    add senha segura

--- src/models/usuarioModel.js
+++ src/models/usuarioModel.js
@@ -1,3 +1,5 @@
+const md5 = require('md5')
+
 const usuarios = [];
 
 function adicionarUsuario(nome, email, senha) {
@@ -6,7 +8,7 @@ function adicionarUsuario(nome, email, senha) {
         id:Date.now(),
         nome: nome,
         email: email,
-        senha: senha,  
+        senha: md5(senha),  
         criadoEm: new Date()
 
     })
